f61313e993c564b240de30ff477fd2c9ffb8e5a6
Marina <marinakovac97@hotmail.com>
2020-03-26 22:22:34 +0100

    changed users interface

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,12 @@ var cors = require('cors');
 
 /* GET users listing. */
 router.get('/', cors(), function(req, res, next) {
-  res.json({users: [{name: 'Timmy'}]});
+  res.json({users: [
+    {
+      username: "username",
+      password: "password"
+    }
+  ]});
 });
 
 module.exports = router;
