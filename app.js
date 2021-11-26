6be1ac8666d391a8a946c117ccbd8c2d752f500a
= <=>
2021-11-26 21:54:35 +0100

    web fix

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var io = app.io = require('socket.io')();
 
 io.opts = {
     cors: {
-      origin: "*",
+      origin: ["https://dixit-online-game.herokuapp.com"],
       credentials:true,
     //   allowedHeaders: ['Content-Type', 'Authorization']
     }
